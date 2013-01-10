dd0688088e654945c905bf5c42b6704bd887eb92
psmits <thestinkypete@gmail.com>
2013-01-09 18:20:21 -0600

    logistic plotting. kind of lame because of small sample size.

--- src/fish_plotting.r
+++ src/fish_plotting.r
@@ -83,6 +83,7 @@ gg.pca <- lapply(scores, function(x) ggpairs(x,
 fish.melt <- reshape2::melt(cbind(rownames(fish.test), fish.test))
 fish.probs.melt <- reshape2::melt(cbind(rownames(fish.pred.prob), fish.pred.prob))
 fish.melts <- fish.probs.melts <- data.frame()
+
 for (ii in seq(length(unique(fish.probs.melt$variable)))) {
   fish.melts <- rbind(fish.melts, fish.melt)
 }
@@ -91,12 +92,12 @@ colnames(fish.melts) <- c('id', 'real', 'PC', 'eigenscore')
 fish.probs.melt <- split(fish.probs.melt, f = fish.probs.melt$variable)
 fish.probs.melts <- fish.probs.melt
 for (jj in seq(length(unique(fish.melt$variable)) - 1)) {
-  ## this isn't working correctly
   ## need to split and then combine with likes
-  ## currently fucks up the plot
   fish.probs.melts <- Map(rbind, fish.probs.melts, fish.probs.melt)
 }
+fish.probs.melts <- Reduce(rbind, fish.probs.melts)
 colnames(fish.probs.melts) <- c('id', 'estimate', 'probability')
+
 fish.super.melt <- cbind(fish.melts, fish.probs.melts)
 fish.super.melt <- fish.super.melt[, -5]
 fish.super.melt <- fish.super.melt[(fish.super.melt$PC == 'PC1' |
@@ -105,6 +106,7 @@ fish.super.melt <- fish.super.melt[(fish.super.melt$PC == 'PC1' |
 gf <- ggplot(fish.super.melt, aes(x = eigenscore, y = probability, colour = estimate))
 gf <- gf + geom_line()
 gf <- gf + facet_wrap(~ PC, scales = 'free')
+gf
 ## we have problems....not getting probabilities for all the variables at the same time
 
 ## cluster plots
