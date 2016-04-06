877261ef7b9fdff3fbc15c715c9acabbd7c2a694
psmits <thestinkypete@gmail.com>
2016-04-06 14:34:32 -0500

    better digitization error code. still unsure how to interpret results

--- R/digit_error.r
+++ R/digit_error.r
@@ -5,13 +5,28 @@ source('../R/supervised_mung.r')
 
 by.scheme <- list()
 scheme <- c('sh1', 'sh2', 'sh3', 'sh4', 'sh5', 'spinks')
+
+rep.turt <- read.table('../data/replicate turtles fixed.txt', 
+                       header = FALSE, stringsAsFactors = FALSE)
+rep.turt <- df2array(rep.turt, n.land = 26, n.dim = 2)
+
+combined <- abind(land.adult, rep.turt)
+combo.turt <- procGPA(combined)
+
+adult$sh1
+
+unit <- rep(c('r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9', 'r0'), 
+            each = 5)
+types <- c(scheme, unique(unit))
+
 for(jj in seq(6)) {
   by.class <- list()
   sch <- as.character(adult[, scheme[jj]])
+  sch <- c(sch, unit)
   sch.t <- unique(sch)
   for(ii in seq(length(sch.t))) {
     sch.w <- sch == sch.t[ii]
-    by.class[[ii]] <- fit$rotated[, , sch.w]
+    by.class[[ii]] <- combo.turt$rotated[, , sch.w]
   }
   by.scheme[[jj]] <- by.class
 }
@@ -28,37 +43,20 @@ for(ii in seq(length(by.scheme))) {
   }
   class.dist[[ii]] <- distmat
 }
-# matrix of distances between centroids of schemes
 
-
-
-rep.turt <- read.table('../data/replicate turtles fixed.txt', 
-                       header = FALSE, stringsAsFactors = FALSE)
-rep.turt <- df2array(rep.turt, n.land = 26, n.dim = 2)
-
-unit <- seq(1, 50, by = 5)
-
-gpa.turt <- list()
-for(ii in seq(length(unit))) {
-  sel <- unit[ii]:(unit[ii] + 4)
-  gpa.turt[[ii]] <- procGPA(rep.turt[, , sel])$rotated
+sch.md <- rep.md <- c()
+sch.dd <- rep.dd <- list()
+for(ii in seq(length(by.scheme))) {
+  sch <- as.character(adult[, scheme[ii]])
+  sch.t <- length(unique(sch))
+  
+  sch.dist <- class.dist[[ii]][seq(sch.t), seq(sch.t)]
+  sch.dd[[ii]] <- sch.dist
+  sch.md[ii] <- mean(sch.dist[lower.tri(sch.dist)])
+  
+  ee <- nrow(class.dist[[ii]])
+  ss <- ee - sch.t
+  rep.dist <- class.dist[[ii]][seq(ss + 1, ee), seq(ss+1, ee)]
+  rep.dd[[ii]] <- rep.dist
+  rep.md[ii] <- mean(rep.dist[lower.tri(rep.dist)])
 }
-
-#riemdist(rep.turt[, , 1], rep.turt[, , 2])
-#ssriemdist(rep.turt[, , 1], rep.turt[, , 2])  # scaled
-within.dist <- list()
-for(ii in seq(length(unit))) {
-  confi <- gpa.turt[[ii]]
-  distmat <- matrix(nrow = length(sel), ncol = length(sel))
-  for(jj in seq(length(sel))) {
-    for(kk in seq(length(sel))) {
-      j1 <- confi[, , jj]
-      k1 <- confi[, , kk]
-      distmat[jj, kk] <- sum(rowSums((j1 - k1)^2))
-    }
-  }
-  within.dist[[ii]] <- distmat
-}
-
-avg.rep <- laply(within.dist, function(x) mean(x[lower.tri(x)]))  # replicate
-avg.class <- laply(class.dist, function(x) mean(x[lower.tri(x)]))  # observed
